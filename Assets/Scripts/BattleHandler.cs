92ae95629d176c036366b237c6f56d3f3d1ae8c4
Bm2mhc <marinus@laurabo.dk>
2020-04-10 23:28:00 +0200

    dobydo

---
+++ Assets/Scripts/BattleHandler.cs
@@ -0,0 +1,191 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHandler : MonoBehaviour {
+
+    private static BattleHandler instance;
+
+    public static BattleHandler GetInstance() {
+        return instance;
+    }
+
+
+    [SerializeField] private Transform pfCharacterBattle;
+    public Texture2D playerSpritesheet;
+    public Texture2D enemySpritesheet;
+
+    private CharacterBattle playerCharacterBattle;
+    private CharacterBattle enemyCharacterBattle;
+    private CharacterBattle activeCharacterBattle;
+    public CharacterBattle other;
+    private State state;
+    //public Text textObject;
+    public int level;
+
+    private enum State {
+        WaitingForPlayer,
+        Busy,
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+
+    public void Update()
+    {
+
+        if (UnityEngine.Random.Range(1, 10) < 8)
+        {
+            attacksimple();
+           // textObject.text = "level " + playerCharacterBattle.level;
+          //  level = other.level;
+            //Debug.Log(level);
+        } else
+        {
+            Heal();
+            //textObject.text = "level " + playerCharacterBattle.level;
+           // level = other.level;
+           // Debug.Log(level);
+        }
+    }
+    private void Start() {
+        playerCharacterBattle = SpawnCharacter(true);
+        enemyCharacterBattle = SpawnCharacter(false);
+
+        SetActiveCharacterBattle(playerCharacterBattle);
+        state = State.WaitingForPlayer;
+    }
+
+    public void attacksimple() {
+        if (state == State.WaitingForPlayer) {
+            
+                state = State.Busy;
+                playerCharacterBattle.Attack(enemyCharacterBattle, () => {
+                    ChooseNextActiveCharacter();
+                });
+        }
+    }
+
+    public void attackother()
+    {
+            playerCharacterBattle.Attack(enemyCharacterBattle, () => {
+                ChooseNextActiveCharacter();
+            });
+    }
+
+    public void Heal()
+    {
+        if (state == State.WaitingForPlayer)
+        {
+
+            state = State.Busy;
+            playerCharacterBattle.heal(playerCharacterBattle, () =>
+        {
+            ChooseNextActiveCharacter();
+        });
+        }
+    }
+
+    public void Healother()
+    {
+            playerCharacterBattle.heal(playerCharacterBattle, () =>
+        {
+            ChooseNextActiveCharacter();
+        });
+        
+    }
+
+    public void upgradedamage()
+    {
+        playerCharacterBattle.upgradedamage();
+    }
+
+
+    private CharacterBattle SpawnCharacter(bool isPlayerTeam) {
+        Vector3 position;
+        if (isPlayerTeam) {
+            position = new Vector3(-50, 0);
+        } else {
+            position = new Vector3(+50, 0);
+        }
+        Transform characterTransform = Instantiate(pfCharacterBattle, position, Quaternion.identity);
+        CharacterBattle characterBattle = characterTransform.GetComponent<CharacterBattle>();
+        characterBattle.Setup(isPlayerTeam);
+
+        return characterBattle;
+    }
+
+    private void SetActiveCharacterBattle(CharacterBattle characterBattle) {
+        if (activeCharacterBattle != null) {
+            activeCharacterBattle.HideSelectionCircle();
+        }
+
+        activeCharacterBattle = characterBattle;
+        activeCharacterBattle.ShowSelectionCircle();
+    }
+
+    private void ChooseNextActiveCharacter() {
+        if (TestBattleOver()) {
+            return;
+        }
+
+        if (activeCharacterBattle == playerCharacterBattle) {
+            SetActiveCharacterBattle(enemyCharacterBattle);
+            state = State.Busy;
+
+            if (UnityEngine.Random.Range(1, 10) < 9)
+            {
+                enemyCharacterBattle.AttackEnemy(playerCharacterBattle, () =>
+                {
+                    ChooseNextActiveCharacter();
+                });
+            }else
+            {
+                enemyCharacterBattle.heal(enemyCharacterBattle, () =>
+                {
+                    ChooseNextActiveCharacter();
+                });
+            }
+
+        } else {
+            SetActiveCharacterBattle(playerCharacterBattle);
+            state = State.WaitingForPlayer;
+        }
+    }
+
+    private bool TestBattleOver() {
+       if (playerCharacterBattle.IsDead()) {
+            // Player dead, enemy wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Enemy Wins!");
+            //BattleOverWindow.Show_Static("Enemy Wins!");
+            playerCharacterBattle.enemyWins();
+           // textObject.text = "level " + playerCharacterBattle.level;
+            // return true;
+        }
+        if (enemyCharacterBattle.IsDead()) {
+            // Enemy dead, player wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Player Wins!");
+            //BattleOverWindow.Show_Static("Player Wins!");
+            enemyCharacterBattle.playerWins();
+          //  textObject.text = "level " + playerCharacterBattle.level;
+
+            //return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/leveltext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class leveltext : CharacterBattle
+{
+    // Start is called before the first frame update
+    public Text textObject;
+    // CharacterBattle info;
+    void Start()
+    {
+        Debug.Log("hej");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Debug.Log(level);
+        changeText();
+
+
+    }
+
+    public void changeText()
+    {
+
+        textObject.text = "level " + level;
+       
+    }
+}
